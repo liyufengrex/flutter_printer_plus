c9faf03d7b3e2ab4716ce8cfa3cf1f7e6e9c3d10
liyufeng1 <liyufeng1@pin-dao.cn>
2023-11-27 10:46:32 +0800

    修改windows_usb版本

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -7,11 +7,8 @@
 #include "generated_plugin_registrant.h"
 
 #include <r_get_ip/r_get_ip_plugin.h>
-#include <windows_usb_printer/windows_usb_printer_provider.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   RGetIpPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("RGetIpPlugin"));
-  WindowsUsbPrinterProviderRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("WindowsUsbPrinterProvider"));
 }
--- example/windows/flutter/generated_plugins.cmake
+++ example/windows/flutter/generated_plugins.cmake
@@ -4,7 +4,6 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   r_get_ip
-  windows_usb_printer
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
